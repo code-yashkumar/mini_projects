82bdeb1273025b93127e67e651b95e2f9b790569
code-yashkumar <code.yashkumar@gmail.com>
2025-12-19 04:26:20 +0530

    Structure Enhanced

---
+++ rock-paper-scissors/script.js
@@ -0,0 +1,22 @@
+let userScore=0;
+let compScore=0;
+
+const choices =document.querySelectorAll('.container');
+
+const compChoice=()=>{
+    const choiceArray=["rock","paper","scissors"];
+    const randomIndex=Math.floor(Math.random()*3);
+    return choiceArray[randomIndex];
+};
+
+const playGame= (userChoice)=>{
+    console.log("User choice is: "+userChoice);
+    //Generate computer choice
+    const computerChoice= compChoice();
+    console.log("Computer choice is: "+computerChoice);
+};
+
+choices.forEach(choice => choice.addEventListener('click', ()=>{
+    const userChoice= choice.id;
+    playGame(userChoice);
+}
